ee4c78eda5c341bdf204c1aa0fe384e31383eef3
BrunoF-M <bruno.freitas.marmol77.bm@gmail.com>
2020-11-27 23:29:57 -0300

    Resolução Exercício

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Divisao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+
+            double numerador;
+            double denominador;
+
+            Console.Write("Digite um numerador!: ");
+            numerador = double.Parse(Console.ReadLine());
+
+
+            Console.Write("\nDigite um Denominador!: ");
+            denominador = double.Parse(Console.ReadLine());
+
+            if (denominador ==0)
+            {
+                Console.WriteLine("\nnão é possivel dividir por 0");
+                Console.WriteLine();
+                Console.Write("\nPress enter to finish...");
+                Console.ReadKey();
+                Console.Clear();
+                Environment.Exit(-1);
+            }
+
+            double resu1 = numerador/denominador;
+
+            Console.WriteLine($"\n{numerador} dividido por {denominador} é {resu1}");
+ 
+            Console.WriteLine();
+            Console.Write("\nPress enter to finish...");
+            Console.ReadKey();
+            Console.Clear();
+        }
+    }
+}
